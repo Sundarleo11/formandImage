705c885f500ec8cb68f6aa96145d911e50db5ca9
Sundarleo11 <sundarrock1999@gmail.com>
2022-11-13 22:54:47 +0530

    postform

--- index.js
+++ index.js
@@ -1,31 +1,39 @@
-const express =require('express');
+const express = require('express');
 
-const app=express();
+const app = express();
 
 //middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 
 app.set("view engine", "ejs");
-app.get("/getpost",(req,res)=>{
-  
-    
+
+app.post("/mypost", (req, res) => {  
+    console.log(req.body);
+    res.status(200).send(req.body); 
+
+});
+app.get("/getpost", (req, res) => {
+
+
     //console.log(req.query);
     console.log(req.body);
     //res.status(200).send(req.body); //postman will work
-   res.status(200).send(req.query); //application will work
-  
+    res.status(200).send(req.query); //application will work
+
 
 });
-app.get("/getform",(req,res)=>{
+
+
+app.get("/getform", (req, res) => {
     res.render("getform");
 })
-app.get("/postform",(req,res)=>{
+app.get("/postform", (req, res) => {
     res.render("postform");
 });
 
 
-app.listen(4000,()=>{
+app.listen(4000, () => {
     console.log("All server are up and runing 4000");
 })
