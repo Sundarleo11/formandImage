ce7375f3ba0601211b6c63ce54a08fec3c8c118f
Sundarleo11 <sundarrock1999@gmail.com>
2022-11-13 22:14:27 +0530

    bugform

--- index.js
+++ index.js
@@ -8,10 +8,15 @@ app.use(express.urlencoded({ extended: true}));
 
 
 app.set("view engine", "ejs");
-app.get("/mypost",(req,res)=>{
+app.get("/getpost",(req,res)=>{
   
+    
+    //console.log(req.query);
     console.log(req.body);
-    res.status(200).send(req.body);
+    //res.status(200).send(req.body); //postman will work
+   res.status(200).send(req.query); //application will work
+  
+
 });
 app.get("/getform",(req,res)=>{
     res.render("getform");
