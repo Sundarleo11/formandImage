c0895c69cd56833efd9b08623df2aa2252e62414
Sundarleo11 <sundarrock1999@gmail.com>
2022-11-13 21:32:38 +0530

    addedforms

--- index.js
+++ index.js
@@ -5,11 +5,22 @@ const app=express();
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
+
+
+app.set("view engine", "ejs");
 app.get("/mypost",(req,res)=>{
   
     console.log(req.body);
     res.status(200).send(req.body);
 });
+app.get("/getform",(req,res)=>{
+    res.render("getform");
+})
+app.get("/postform",(req,res)=>{
+    res.render("postform");
+});
+
+
 app.listen(4000,()=>{
     console.log("All server are up and runing 4000");
 })
