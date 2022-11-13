4456a9a6e3bce23c7ba95fd8afdc39bcf389ca9d
Sundarleo11 <sundarrock1999@gmail.com>
2022-11-13 21:08:49 +0530

    urlencode

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ const express =require('express');
 
 const app=express();
 
-
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.get("/mypost",(req,res)=>{
+  
+    console.log(req.body);
+    res.status(200).send(req.body);
+});
 app.listen(4000,()=>{
     console.log("All server are up and runing 4000");
 })
