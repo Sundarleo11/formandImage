6a6d194bde23c3e246d6deb6708b19ab48735152
Sundarleo11 <sundarrock1999@gmail.com>
2022-11-16 10:21:47 +0530

    cloudinary

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 const express = require('express');
-
+const files=require('express-fileupload');
+const cloudinary=require('cloudinary').v2;
 const app = express();
-
+cloudinary.config({
+    cloud_name:"dmyqjit0z",
+    api_key:"193976785214388",
+    api_secret:"hFbAO8UjRCj58GDoOxx-jSHjvok"
+});
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(files({
+    useTempFiles: true,
+    tempFileDir:"/tmp",
+}));
 
 app.set("view engine", "ejs");
 
-app.post("/mypost", (req, res) => {  
+app.post("/mypost", async (req, res) => {  
     console.log(req.body);
-    res.status(200).send(req.body); 
+    console.log(req.files);
+ const file=req.files.savefile;
+ 
+ result=await cloudinary.uploader.upload(file.tempFilePath,{
+  folder:"user"
+ })
+ console.log(result);
+    details={
+        firstname:req.body.Firstname,
+        lastname:req.body.LastName,
+        result
+    }
+    //res.status(200).send(req.body); 
+    res.status(200).send(details);
 
 });
 app.get("/getpost", (req, res) => {
